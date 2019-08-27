4cd05ab5b780f2c82bf0cc7bd1eb8e09452823a7
gillchristian <gillchristiang@gmail.com>
2019-08-27 08:15:46 +0200

    support(api): Fix mutation bug

--- src/projections.ts
+++ src/projections.ts
@@ -12,38 +12,39 @@ const between = (from: number, to: number) => (x: number) => {
 
 const itemRange = between(0, 1000)
 
-const EMPTY_CART: Cart = {totalPrice: 0, items: {}, discount: undefined}
-
 export const cart = (items: Item[], discounts: Discount[]) => (
   events: Event[],
 ): Cart => {
-  const cartProjection: Cart = events.reduce((accCart, {mutation}) => {
-    switch (mutation.type) {
-      case MT.AddItem:
-      case MT.UpdateItem: {
-        const {count} = mutation
-        const item = accCart.items[mutation.sku]
+  const cartProjection: Cart = events.reduce(
+    (accCart: Cart, {mutation}: Event) => {
+      switch (mutation.type) {
+        case MT.AddItem:
+        case MT.UpdateItem: {
+          const {count} = mutation
+          const item = accCart.items[mutation.sku]
 
-        accCart.items[mutation.sku] = itemRange(item ? item + count : count)
+          accCart.items[mutation.sku] = itemRange(item ? item + count : count)
 
-        return accCart
+          return accCart
+        }
+
+        case MT.DeleteItem: {
+          accCart.items[mutation.sku] = undefined
+
+          return accCart
+        }
+
+        case MT.EmptyCart: {
+          return {totalPrice: 0, items: {}, discount: undefined}
+        }
+
+        case MT.ApplyDiscount: {
+          return {...accCart, discount: mutation.code}
+        }
       }
-
-      case MT.DeleteItem: {
-        accCart.items[mutation.sku] = undefined
-
-        return accCart
-      }
-
-      case MT.EmptyCart: {
-        return EMPTY_CART
-      }
-
-      case MT.ApplyDiscount: {
-        return {...accCart, discount: mutation.code}
-      }
-    }
-  }, EMPTY_CART)
+    },
+    {totalPrice: 0, items: {}, discount: undefined},
+  )
 
   const totalPrice = Object.entries(cartProjection.items).reduce(
     (acc, [sku, count]) => {
